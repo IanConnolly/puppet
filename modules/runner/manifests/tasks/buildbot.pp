f426eb480d8699bd6673a5f0fe700112a466e1d5
Ian Connolly <iconnolly@mozilla.com>
2014-07-09 13:29:18 -0700

    C+P fail

--- modules/runner/manifests/tasks/buildbot.pp
+++ modules/runner/manifests/tasks/buildbot.pp
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 # Make sure runner runs at boot
-class runner::tasks::update_shared_repos($runlevel=4) {
+class runner::tasks::buildbot($runlevel=4) {
     #include packages::mozilla::psutil
     include runner
 
